976d82bd4ec622cf16c54f58ac38fd089ba0c763
christopherbarbarie <christopher.barbarie@mail.mcgill.ca>
2024-09-27 14:38:14 +0000

    as close as its gonna get

--- HW4/borough_complaints.py
+++ HW4/borough_complaints.py
@@ -1,13 +1,40 @@
-import numpy as np
 import pandas as pd
-import argparse as ap
-import sys
-import os
+import argparse
 
-parser = ap.ArgumentParser(description='Count complaints by borough')
-parser.add_argument('-i', help='Name of the input file', required=True)
-parser.add_argument('-s', help='Start date for filtering', required=True)
-parser.add_argument('-e', help='End date for filtering', required=True)
-parser.add_argument('-o',help='Name of the output file')
+parser = argparse.ArgumentParser()
+parser.add_argument('-i', help='Name of the input file')
+parser.add_argument('-s', help='Start date')
+parser.add_argument('-e', help='End date')
+parser.add_argument('-o', help='Name of the output file')
 args = parser.parse_args()
 
+def parse_date(date_str):
+    for fmt in ('%m/%d/%Y %I:%M:%S %p', '%m/%d/%Y'):
+        try:
+            return pd.to_datetime(date_str, format=fmt)
+        except ValueError:
+            continue
+    raise ValueError(f"Date format for {date_str} not recognized")
+
+start_date = parse_date(args.s)
+end_date = parse_date(args.e)
+
+chunksize = 1000 
+chunks = []
+
+for chunk in pd.read_csv(args.i, chunksize=chunksize, usecols=[2, 3, 6, 17]):
+    chunk.columns = ['start date', 'end date', 'complaint type', 'borough']
+    chunk['start date'] = pd.to_datetime(chunk['start date'], format='%m/%d/%Y %I:%M:%S %p', errors='coerce')
+    chunk['end date'] = pd.to_datetime(chunk['end date'], format='%m/%d/%Y %I:%M:%S %p', errors='coerce')
+    
+    filtered_chunk = chunk[(chunk['start date'] >= start_date) & (chunk['start date'] <= end_date)]
+    chunks.append(filtered_chunk)
+
+if chunks:
+    data = pd.concat(chunks)
+    complaint_counts = data.groupby(['complaint type', 'borough']).size().reset_index(name='count')
+
+    if args.o:
+        complaint_counts.to_csv(args.o, index=False)
+    else:
+        print(complaint_counts)
